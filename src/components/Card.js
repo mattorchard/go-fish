07165ac03e7dcc2f97c497db441604e58f45f8a1
morch028 <morch028@uottawa.ca>
2019-09-07 16:08:06 -0400

    Adds suit icon under rank on all cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -15,9 +15,19 @@ const Pips = ({rank, suit}) => <div className={`pips pips--${rank}`}>{
 const Card = ({suit, rank, flipped=false}) => <div className={`card ${flipped && "card--flipped"}`}>
   <div className="card__back"/>
   <div className={`card__face ${suit}`}>
-      <div className="rank">{rank}</div>
-      {isFace(rank) ? <FaceCardArt suit={suit} rank={rank}/> : <Pips suit={suit} rank={rank}/>}
-      <div className="rank">{rank}</div>
+      <div className="rank">
+        {rank}
+        <SuitIcon suit={suit} />
+      </div>
+
+    {isFace(rank)
+      ? <FaceCardArt suit={suit} rank={rank}/>
+      : <Pips suit={suit} rank={rank}/>}
+
+      <div className="rank">
+        {rank}
+        <SuitIcon suit={suit} />
+      </div>
   </div>
 </div>;
 

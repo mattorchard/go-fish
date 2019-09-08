31c978b4b9ca16475cc68631a9fa0ee964b9b2fa
morch028 <morch028@uottawa.ca>
2019-09-08 13:45:00 -0400

    Adds vanilla + hook game engine

--- src/App.js
+++ src/App.js
@@ -4,46 +4,18 @@ import Hand from "./components/Hand";
 import OpponentHand from "./components/OponentHand";
 import DrawPile from "./components/DrawPile";
 import Pairs from "./components/Pairs";
-
-
-const defaultHand = [
-  {suit: "hearts", rank: "A"},
-  {suit: "diamonds", rank: 2},
-  {suit: "clubs", rank: 3},
-  {suit: "hearts", rank: 4},
-  {suit: "clubs", rank: 5},
-  {suit: "spades", rank: 6},
-  {suit: "hearts", rank: 7},
-  {suit: "spades", rank: 8},
-  {suit: "spades", rank: 9},
-  {suit: "hearts", rank: 10},
-  {suit: "clubs", rank: "J"},
-  {suit: "diamonds", rank: "Q"},
-  {suit: "spades", rank: "K"},
-];
-
-const pairsExample = [
-  [
-    {suit: "diamonds", rank: 7},
-    {suit: "hearts", rank: 7}
-  ],
-  [
-    {suit: "clubs", rank: 8},
-    {suit: "hearts", rank: 8}
-  ],
-  [
-    {suit: "clubs", rank: "Q"},
-    {suit: "spades", rank: "Q"}
-  ]
-];
+import useGoFish from "./helpers/use-go-fish";
 
 function App() {
+  const {chooseRank, playerCards, opponentCards, playerPairs, opponentPairs, drawPileSize} = useGoFish();
   return <>
-    <OpponentHand cards={defaultHand}/>
-    <Pairs pairs={pairsExample}/>
-    <DrawPile cardCount={0}/>
-    <Pairs pairs={pairsExample}/>
-    <Hand cards={defaultHand}/>
+    <OpponentHand cards={opponentCards}/>
+    <div style={{display: "flex"}}>
+    <Pairs pairs={opponentPairs}/>
+    <DrawPile cardCount={drawPileSize}/>
+    <Pairs pairs={playerPairs}/>
+    </div>
+    <Hand cards={playerCards} onChooseRank={chooseRank}/>
   </>;
 }
 
--- src/components/Hand.js
+++ src/components/Hand.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import Card from "./Card";
 import "./Hand.css";
 
 
-const Hand = ({cards, disabled = false}) => {
+const Hand = ({cards, onChooseRank}) => {
+  const [disabled, setDisabled] = useState(false);
 
   return <form className="hand">
+    {disabled && "Waiting"}
     {cards.map(({suit, rank}) =>
       <button
         key={`${rank}-${suit}`}
+        onClick={async () => {
+          setDisabled(true);
+          await onChooseRank(rank);
+          setDisabled(false);
+        }}
         type="button"
         className="hand__slot"
         disabled={disabled}>

07580aba06f6c092a9b77014e2fd7d5b5257227a
Matthew <morch028@uottawa.ca>
2019-09-17 17:55:04 -0400

    Improves hand styles

--- src/components/OponentHand.js
+++ src/components/OponentHand.js
@@ -5,15 +5,15 @@ import Repeat from "./Repeat"
 const OpponentHand = ({cardCount}) => <ol
   className="opponent-hand"
   style={{"--child-count": cardCount}}>
-  {
-  <Repeat count={cardCount} render={(index =>
-    <li
-      className="opponent-hand__slot"
-      style={{ "--child-index": index }}>
-      <CardBack
-        size="medium"
-        flipped/>
-    </li>
-  )}/>}</ol>;
+  {<Repeat count={cardCount} render={(index =>
+      <li
+        className="opponent-hand__slot"
+        style={{"--child-index": index}}>
+        <CardBack
+          size="medium"
+          flipped/>
+      </li>
+  )}/>}
+</ol>;
 
 export default OpponentHand;

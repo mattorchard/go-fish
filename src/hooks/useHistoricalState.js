c87cc8477b94dac0a5cbccfa457675716c56ebb6
Matthew <morch028@uottawa.ca>
2019-09-17 17:04:06 -0400

    Fix exhaustive dependency violation

--- src/hooks/useHistoricalState.js
+++ src/hooks/useHistoricalState.js
@@ -5,9 +5,12 @@ const useHistoricalState = value => {
   const [history, setHistory] = useState([value]);
 
   useEffect(() => {
-    if (value !== history[0]) {
-      setHistory([value, ...history]);
-    }
+    setHistory(history => {
+      if (value !== history[0]) {
+        return [value, ...history];
+      }
+      return history
+    })
   }, [value]);
 
   return history;
